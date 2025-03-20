8ee184df2c9b9dc2318ccc32da41b150c9ad2645
Pournima Choulwar <pornimachoulwar@gmail.com>
2025-03-20 22:26:25 +0530

    Update GPIO_toggle_code.c

--- No_Serial_Debugging?Use_GPIO_Pins_Instead!/GPIO_toggle_code.c
+++ No_Serial_Debugging?Use_GPIO_Pins_Instead!/GPIO_toggle_code.c
@@ -1,7 +1,27 @@
 // Below Register-Level Programming can be used as a reference to toggle your gpio pin to debug an error
 
-//Function to  toggle pin
+#include "stm32f4xx.h"  // Change according to your STM32 series
+
+// Function to initialize GPIO pin as output
+void gpio_init()
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;  // Enable clock for GPIOA (Change GPIOx as needed)
+    GPIOA->MODER |= (1 << (5 * 2));       // Set PA5 as output (Change pin as needed)
+}
+
+/ Function to toggle GPIO pin
 void gpio_toggle()
 {
-  
+    GPIOA->ODR ^= (1 << 5);  // Toggle PA5 (Change pin as needed)
+}
+
+int main()
+{
+    gpio_init();
+
+    while (1)
+    {
+        gpio_toggle();
+        for (volatile int i = 0; i < 1000; i++);  // Delay
+    }
 }
