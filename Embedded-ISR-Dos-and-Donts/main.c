53acafa12dc4d103604dc7ed5d7608b9a2cd1279
Pournima Choulwar <pornimachoulwar@gmail.com>
2025-05-05 17:21:14 +0530

    Update main.c

--- Embedded-ISR-Dos-and-Donts/main.c
+++ Embedded-ISR-Dos-and-Donts/main.c
@@ -6,3 +6,5 @@ void ISR_Handler()
     int data[10]  ;   // Local variable initialized
     process_data()  ; // Calling a function - Big mistake
 }
+
+//Seems harmless, right? But this adds stack overhead and delays your ISR exit
